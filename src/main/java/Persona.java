e825bc50493239026a16a6b5f604028548cd0f7d
grafijoss <grafijoss@hotmail.com>
2020-04-27 16:35:07 -0500

    Creacion clase

---
+++ src/main/java/Persona.java
@@ -0,0 +1,14 @@
+public class Persona {
+    // Atributos
+    String nombre;
+    String apellidos;
+    
+    // Metodo de la clase
+    // Lo usaran los objetos de esta clase
+    
+    public void desplegarNombre() {
+        System.out.ptintln("Nombre" + nombre);
+        System.out.ptintln("Apellido" + apellido);
+    }
+
+}
