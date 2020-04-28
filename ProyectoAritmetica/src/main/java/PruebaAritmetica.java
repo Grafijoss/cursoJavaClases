956d011c4a781cb7813417e57db1fe3c099cd629
grafijoss <grafijoss@hotmail.com>
2020-04-27 20:30:42 -0500

    Creacion de metodos en Java ProyectoAritmetica

---
+++ ProyectoAritmetica/src/main/java/PruebaAritmetica.java
@@ -0,0 +1,15 @@
+public class PruebaAritmetica {
+    
+    // psvm
+    // sout
+    public static void main(String[] args) {
+        
+        // Creamos un objeto de tipo aritmetica
+        Aritmetica aritmetica = new Aritmetica();
+        
+        int resultado = aritmetica.sumar(5, 5);
+        System.out.println("resultado = " + resultado);
+        
+        
+    }
+}
